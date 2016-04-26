7ed74ef0a3325c9c8fc9906a3ebf71fe7b6859aa
gettogetto <643274003@qq.com>
2016-04-26 12:22:03 +0800

    code

--- 4_2.c
+++ 4_2.c
@@ -4,11 +4,11 @@
 int main(int argc,char* argv[]){
     if(argc!=2)
         err_quit("error:argc!=2");
-    if(access(argv[1],R_OK)==-1)
+    if(access(argv[1],R_OK)==-1)//实际 用户id和组 id
         err_ret("access error for %s",argv[1]);
     else 
         printf("read access ok\n");
-    if(open(argv[1],O_RDONLY)==-1)
+    if(open(argv[1],O_RDONLY)==-1)//有效用户id和组 id
         err_ret("open error for %s",argv[1]);
     else 
        printf("read open ok\n");
