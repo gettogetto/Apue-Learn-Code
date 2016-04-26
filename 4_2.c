3b303e1bb562041c026dbd7f64a4fbc0afcf85e8
gettogetto <643274003@qq.com>
2016-04-26 12:20:35 +0800

    code

---
+++ 4_2.c
@@ -0,0 +1,16 @@
+#include<sys/types.h>
+#include<fcntl.h>
+#include"ourhdr.h"
+int main(int argc,char* argv[]){
+    if(argc!=2)
+        err_quit("error:argc!=2");
+    if(access(argv[1],R_OK)==-1)
+        err_ret("access error for %s",argv[1]);
+    else 
+        printf("read access ok\n");
+    if(open(argv[1],O_RDONLY)==-1)
+        err_ret("open error for %s",argv[1]);
+    else 
+       printf("read open ok\n");
+    exit(0);
+}
